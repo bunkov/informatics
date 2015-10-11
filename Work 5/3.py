502db66f326de3b3aa2fe107dd1a149d7281804d
bunkov <bunk.alexey@gmail.com>
2015-10-11 21:40:49 +0300

    Практическая работа №5

---
+++ Work 5/3.py
@@ -0,0 +1,9 @@
+#Exercise 3.1
+A=[1,2,3,4,5]
+print(' '.join(map(str,A[::2])))
+#Exercise 3.2
+A=[1,2,3,2,1]
+print(max(A),A.index(max(A)))
+#Exercise 3.3
+A=[1,2,3,4,5]
+print(' '.join(map(str,A[::-1])))
