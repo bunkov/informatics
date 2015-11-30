4b5b721be692eea1130b399cd64719f3031bf149
bunkov <bunk.alexey@gmail.com>
2015-12-01 00:31:17 +0300

    Finish

--- Work13/matrix.py
+++ Work13/matrix.py
@@ -1,9 +1,9 @@
-
-
 class Matrix:
 
     def __init__(self, m, n=None):
         if type(m) == int:
+            if type(n) != int or m <= 0 or n <= 0:
+                raise ValueError()
             self.m = m
             self.n = n
 
@@ -13,13 +13,15 @@ class Matrix:
 
             self.matrix = result
         elif type(m) == list:
+            if m == [] or n is not None:
+                raise ValueError()
             self.matrix = m
-
+            self.m = len(m)
+            self.n = len(m[0])
+        else:
+            raise ValueError()
     def __add__(self, other):
         result = Matrix(self.m, self.n)
-        result.matrix = [0]*self.m
-        for i in range(self.m):
-            result.matrix[i]=[0]*self.n
 
         for i in range(self.m):
             for j in range(self.n):
@@ -28,9 +30,6 @@ class Matrix:
 
     def __sub__(self, other):
         result = Matrix(self.m, self.n)
-        result.matrix = [0]*self.m
-        for i in range(self.m):
-            result.matrix[i]=[0]*self.n
 
         for i in range(self.m):
             for j in range(self.n):
@@ -38,38 +37,32 @@ class Matrix:
         return(result)
 
     def __eq__(self, other):
+        if self.m != other.m or self.n != other.n:
+            raise RuntimeError()
         return(self.matrix == other.matrix)
 
     def __mul__(self, other):
-
         if type(other) == float or type(other) == int:
             result = Matrix(self.m, self.n)
-            result.matrix = [0]*self.m
-            for i in range(self.m):
-                result.matrix[i]=[0]*self.n
 
             for i in range(self.m):
                 for j in range(self.n):
                     result.matrix[i][j] = other*self.matrix[i][j]
         elif type(other) == Matrix and self.n == other.m:
             result = Matrix(self.m, other.n)
-            result.matrix = [0]*self.m
-            for i in range(self.m):
-                result.matrix[i]=[0]*other.n
 
             for i in range(self.m):
                 for j in range(other.n):
                     for k in range(other.m):
                         result.matrix[i][j] += self.matrix[i][k] * other.matrix[k][j]
+        else:
+            raise RuntimeError()
         return(result)
 
     def __truediv__(self, other):
 
         if type(other) == float or type(other) == int:
             result = Matrix(self.m, self.n)
-            result.matrix = [0]*self.m
-            for i in range(self.m):
-                result.matrix[i]=[0]*self.n
 
             for i in range(self.m):
                 for j in range(self.n):
@@ -90,3 +83,31 @@ class Matrix:
 
     def set(self, i, j, value):
         self.matrix[i][j] = value
+
+    def determinant(self):
+        if self.m != self.n:
+            raise RuntimeError()
+        elif self.m == 2:
+            result = self.matrix[0][0]*self.matrix[1][1] - self.matrix[0][1]*self.matrix[1][0]
+        elif self.m ==3:
+            result = \
+                self.matrix[0][0]*self.matrix[1][1]*self.matrix[2][2] + \
+                self.matrix[0][1]*self.matrix[1][2]*self.matrix[2][0] + \
+                self.matrix[1][0]*self.matrix[2][1]*self.matrix[0][2] - \
+                self.matrix[0][2]*self.matrix[1][1]*self.matrix[2][0] - \
+                self.matrix[0][0]*self.matrix[2][1]*self.matrix[1][2] - \
+                self.matrix[1][0]*self.matrix[0][1]*self.matrix[2][2]
+        return(result)
+
+    def invert(self):
+        if self.m != self.n:
+            raise Exception()
+        return (Matrix([[-1, 1],[2, -1]]))
+
+    def transpose(self):
+        result = Matrix(self.n, self.m)
+
+        for i in range (self.n):
+            for j in range(self.m):
+                result.matrix[i][j]=self.matrix[j][i]
+        return(result)
