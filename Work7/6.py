da44495cb1ee38e378304274a598ad9ff7b309df
bunkov <bunk.alexey@gmail.com>
2015-12-08 12:15:59 +0300

    It works!

--- Work7/6.py
+++ Work7/6.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy
 import matplotlib.pyplot as plt
 from math import *
 import pylab
@@ -6,20 +6,20 @@ from matplotlib import mlab
 f=open('input.txt','r')
 
 A=' '.join(f.readlines()).split()
-k=[0]*len(A)
+k = [len(a) for a in A]
+'''k=[0]*len(A)
 n=0
 for i in range(len(A)):
-	k[i]=len(A[i])
+	k[i]=len(A[i])'''
 y=[0]*max(k)
 for i in range(max(k)):
-	
 	y[i]=k.count(i+1)
 
 x=[i+1 for i in range(max(k))]
+y_pos = numpy.arange(len(x))
+plt.bar(y_pos, y, align='center', alpha=0.5)
+plt.xticks(y_pos, x)
 
-print(x)
-n, bins, patches = plt.hist(x, 50, normed=1, facecolor='g', alpha=0.75)
-
-plt.axis([0, max(x), 0, max(y)])
-plt.grid(True)
+#plt.axis([0, max(x), 0, max(y)])
+#plt.grid(True)
 plt.show()
