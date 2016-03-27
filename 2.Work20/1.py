76a5a2cea26782bd6f951cb24944e5ea98a0d972
bunkov <bunk.alexey@gmail.com>
2016-03-28 02:55:10 +0300

    Реализовано: считывание графа из файла, DFS, BFS, отображение компонент связности

--- 2.Work20/1.py
+++ 2.Work20/1.py
@@ -3,49 +3,87 @@ import networkx as nx
 
 def get_graph(data_file):
 	G = nx.Graph()
-	#vertices_num = int(input('Number of vertices'))
-	edges_num = int(input('Numbers of edges: '))
+	edges_num = int(data_file.readline().rstrip())
 	data = [0]*edges_num
-	#for i in range (nodes_num):
-		
 	for i in range (edges_num):
-		data[i] = data_file.readline().rstrip()
+		data[i] = data_file.readline().rstrip().split()
 		vertex_1 = data[i][0]
-
-		if len(data[i]) == 1:
-			if vertex_1 not in G:
-				G.add_node(vertex_1)
-		elif len(data[i]) == 3:
-			vertex_2 = data[i][2]
-			G.add_edge(vertex_1, vertex_2, weight = 1)
-		elif len(data[i]) == 5:
-			w = data[i][4]
-			vertex_2 = data[i][2]
+		if len(data[i]) == 1 and vertex_1 not in G:
+			G.add_node(vertex_1)
+		else:
+			vertex_2 = data[i][1]
+			if len(data[i]) == 3:
+				w = data[i][2]
+			else:
+				w = 1
 			G.add_edge(vertex_1, vertex_2, weight = w)
 	return G
 
-def get_path():
+def get_path(G):
 	length = int(input('Length of path: '))
 	edge_list = [0]*length
-	print('edge(vertex 1, vertex 2): ')
+	print('edge(vertex 1, vertex 2):')
 	i = 0
 	while i < (length):
 		print(i+1, end = ': ')
 		edge_list[i] = input().split()
-		if i != 0:
-			if edge_list[i-1][1] !=	edge_list[i][0]:
+		if (edge_list[i][0] not in G.nodes() or edge_list[i][1] not in G.nodes() # Если вершин нет в графе
+			or (i != 0 and edge_list[i-1][1] != edge_list[i][0]) # Если конец пройденного ребра не начало нового
+			or (edge_list[i][0], edge_list[i][1]) not in G.edges() and (edge_list[i][1], edge_list[i][0]) not in G.edges()): # Нет такого ребра
 				print("It isn't path")
 				i -= 1
 		i += 1
 	return edge_list
-		
-data_file = open(input('Name of file: ')+'.txt')
+
+def dfs(G, start, called = [], edge_list = []): # Поиск по глубине
+	called.append(start)
+	for neighbour in G[start]:
+		if neighbour not in called:
+			dfs(G, neighbour, called, edge_list)
+			edge = (start, neighbour)
+			edge_list.append(edge)
+	return edge_list, called
+
+def bfs(G, start, fired = [], edge_list= []): # Поиск по ширине
+	queue = [start]
+	fired.append(start)
+	while queue:
+		current = queue.pop(0)
+		for neighbour in G[current]:
+			if neighbour not in fired:
+				fired.append(neighbour)
+				queue.append(neighbour)
+				edge = (current, neighbour)
+				edge_list.append(edge)
+	return edge_list, fired
+
+data_file = open('2.txt')
+#data_file = open(input('Name of file: ')+'.txt')
 G = get_graph(data_file)
 pos = nx.spring_layout(G)
-nx.draw_networkx_nodes(G,pos,node_size=700)
-nx.draw_networkx_edges(G, pos, G.edges(), width=8, alpha=1, edge_color='b')
-nx.draw_networkx_edges(G, pos, get_path(), width=8, alpha=1, edge_color='r')
-nx.draw_networkx_labels(G,pos,font_size=20,font_family='sans-serif')
+
+# Отрисовываем весь граф
+nx.draw_networkx_nodes(G,pos,node_size=100)
+nx.draw_networkx_edges(G, pos, G.edges(), width=5, alpha=1, edge_color='b')
+nx.draw_networkx_labels(G,pos,font_size=20,font_family='arial')
+
+edge_list, woken = dfs(G, 'Апельсиновый') # Ребра основного дерева
+n = 1 # Кол-во компонент связности
+while len(woken) != len(G.nodes()):
+	for node in G.nodes():
+		if node not in woken:
+			edge_part, woken_part = bfs(G, node)
+			edge_list += edge_part
+			woken += woken_part
+			n += 1
+
+#nx.draw_networkx_edges(G, pos, get_path(G), width=6, alpha=1, edge_color='r') # Отрисовываем путь
+nx.draw_networkx_edges(G, pos, edge_list, width=8, alpha=0.5, edge_color='g') # Отрисовываем основное дерево
+print('Кол-во компонент связности:', n)
+if n == 1:
+	print('Граф связный')
+else:
+	print('Граф несвязный')
 plt.axis('off')
 plt.show()
 data_file.close()
